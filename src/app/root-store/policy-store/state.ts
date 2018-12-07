2b49960686455b0f82ad7b3667a43a7ef60dc386
djangulo <denis.angulo@gmail.com>
2018-12-07 17:56:50 -0400

    added policy state

---
+++ src/app/root-store/policy-store/state.ts
@@ -0,0 +1,32 @@
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import {
+  APIError,
+  Policy,
+} from '../models';
+
+export const featureAdapter: EntityAdapter<
+  Policy
+> = createEntityAdapter<Policy>({
+  selectId: model => model.uuid,
+  sortComparer: (a: Policy, b: Policy): number => {
+    return b.createdAt.toString().localeCompare(a.createdAt.toString());
+  }
+})
+
+export interface State {
+  policies?: Policy[];
+  selectedPolicy?: Policy,
+  formValues: Policy | null,
+  isLoading?: boolean;
+  error: APIError;
+}
+
+export const initialState: State = featureAdapter.getInitialState(
+  {
+    policies: [],
+    selectedInsurancePolicy: null,
+    formValues: null,
+    error: null,
+    isLoading: false
+  }
+);
