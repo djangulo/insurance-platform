b16fe5bbcd444b2f63ceb8c3adef437c6f0cb916
djangulo <denis.angulo@gmail.com>
2018-12-08 10:31:51 -0400

    added index to root-store

---
+++ src/app/root-store/index.ts
@@ -0,0 +1,11 @@
+import { RootStoreModule } from './root-store.module';
+import * as RootStoreSelectors from './selectors';
+import * as RootStoreState from './root-state';
+
+export * from './policies-store';
+
+export {
+  RootStoreModule,
+  RootStoreSelectors,
+  RootStoreState,
+};
