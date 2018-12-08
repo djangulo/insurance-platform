637a4799f0daacd140ae83b693686a35c717c69c
djangulo <denis.angulo@gmail.com>
2018-12-08 11:43:02 -0400

    added configurations to policy.service

--- src/app/services/policy.service.spec.ts
+++ src/app/services/policy.service.spec.ts
@@ -1,9 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import {
+  HttpClient,
+  HttpErrorResponse
+} from '@angular/common/http';
 
 import { PolicyService } from './policy.service';
 
-describe('PolicyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('InsurancePolicyService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PolicyService]
+    });
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
     const service: PolicyService = TestBed.get(PolicyService);
